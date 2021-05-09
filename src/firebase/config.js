4e7cb3591288c59e6d32eda18d0115878aba5009
Adhishreya P <42477290+Adhishreya@users.noreply.github.com>
2021-05-09 14:12:49 +0530

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,12 +1,12 @@
 import firebase from "firebase";
 import "firebase/firestore";
 var firebaseConfig = {
-  apiKey: "AIzaSyDyu6Ysa6UhBCODJ_4n1vJbHYEt7n-r-ow",
-  authDomain: "keep-clone-d694d.firebaseapp.com",
-  projectId: "keep-clone-d694d",
-  storageBucket: "keep-clone-d694d.appspot.com",
-  messagingSenderId: "1053789478141",
-  appId: "1:1053789478141:web:767477e9a32b807ce73294"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
